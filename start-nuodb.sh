7bd86d929d2fce02122f671b6659a87dca519013
Mahesha Godekere <mgodekere@nuodb.com>
2015-05-19 08:41:12 +0530

    initial version
    Script to run NuoDB. Docker uses this script as Entrypoint.

---
+++ start-nuodb.sh
@@ -0,0 +1,8 @@
+echo "Starting nuoagent:"
+/opt/nuodb/etc/nuoagent start
+
+echo "Starting nuodb rest service:"
+/opt/nuodb/etc/nuorestsvc start
+
+echo "Tailing agent logs keep container running!!!"
+tail -n 1000 -f /opt/nuodb/var/log/agent.log
